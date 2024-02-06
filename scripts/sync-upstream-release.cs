b77510f2c93e0f4f387ffa969627e34dd8d3c79e
Weihan Li <weihanli@outlook.com>
2024-02-07 00:22:55 +0800

    Update sync-upstream-release.cs

--- scripts/sync-upstream-release.cs
+++ scripts/sync-upstream-release.cs
@@ -1,4 +1,5 @@
 // https://docs.github.com/en/rest/releases/releases#get-the-latest-release
+using System.Net.Http.Json;
 
 const string previousRelease = "0.17.0";
 
@@ -9,17 +10,22 @@ HttpHelper.HttpClient.DefaultRequestHeaders.TryAddWithoutValidation("X-GitHub-Ap
 var releaseCheckUrl = "https://api.github.com/repos/WeihanLi/dotnet-exec/releases/latest";
 var release = await HttpHelper.HttpClient.GetFromJsonAsync<Release>(releaseCheckUrl);
 
-if (release.Name is previousRelease)
+if (release.TagName is previousRelease)
 {
+    Console.WriteLine($"No new release found after {previousRelease} release");
     return;
 }
 
+Console.WriteLine($"New release found: {release}");
 foreach (var file in Directory.GetFiles(Environment.CurrentDirectory))
 {
 	var sourceText = File.ReadAllText(file);
 	var replacedText = sourceText.Replace(previousRelease, release.Name);
 	if (replacedText == sourceText) continue;
+	
 	await File.WriteAllTextAsync(file, replacedText);
+	Console.WriteLine($"file {file} updated");
 }
 
-public sealed record Release(string Name, string Body);
+
+public sealed record Release(string Name, string TagName, string Body);
